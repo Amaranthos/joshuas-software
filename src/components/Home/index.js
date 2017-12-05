afb93e82a3298fe5900ceddfa79420fac5bcb70c
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-12-05 21:57:20 +1000

    Add Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import Panel from 'muicss/lib/react/Panel'
+
+export default class Home extends Component {
+	render() {
+		return (
+			<Panel></Panel>
+		);
+	}
+}
