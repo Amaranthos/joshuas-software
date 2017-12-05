6c2608d883228f8781500a7b736306e592941092
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-12-05 20:10:56 +1000

    Add firebase settings

---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDDB6IuaN45i8NB24CWOdTd-GVS7XUf6j4",
+    authDomain: "joshuas-software.firebaseapp.com",
+    databaseURL: "https://joshuas-software.firebaseio.com",
+    projectId: "joshuas-software",
+    storageBucket: "joshuas-software.appspot.com",
+    messagingSenderId: "580757283581"
+};
+
+export fire = firebase.initializeApp(config);
