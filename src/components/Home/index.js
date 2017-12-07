6e9ed0efc1663bc05457e28921e42aba9a6903f8
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-12-07 22:45:28 +1000

    Fleshing out Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,9 +14,13 @@ export default class Home extends Component {
 					<li>scripting</li>
 					<li>tools</li>
 				</ul>
-				<div>
-					<a href="https://github.com/Amaranthos"><i className="fa fa-github-square"/></a>
-					<a href="https://bitbucket.org/Amaranthos"><i className="fa fa-bitbucket-square"/></a>
+				<div className="home-links">
+					<a href="https://github.com/amaranthos"><i className="fa fa-github-square"/></a>
+					<a href="https://bitbucket.org/amaranthos"><i className="fa fa-bitbucket-square"/></a>
+					<a href="https://www.youtube.com/channel/UC4VaUP2Ob7I6avrajgBm70w"><i className="fa fa-youtube-square"/></a>
+					<a href="https://www.linkedin.com/in/joshua-hodkinson"><i className="fa fa-linkedin-square"/></a>
+					<a href="https://twitter.com/jbwhodkinson"><i className="fa fa-twitter-square"/></a>
+					<a href="mailto:joshua@joshuas.software"><i className="fa fa-envelope-square"/></a>
 				</div>
 			</div>
 		);
