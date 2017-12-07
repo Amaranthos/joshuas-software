041f9df3474d20ce547df0a9e4354575ae91f152
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-12-07 21:44:20 +1000

    Start implemnting Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import Panel from 'muicss/lib/react/panel'
+import "./style.css";
 
 export default class Home extends Component {
 	render() {
 		return (
-			<div>
-				Hello, world
+			<div className="home">
+				<h3>I write code</h3>
+				<ul>
+					<li>games</li>
+					<li>web</li>
+					<li>front end</li>
+					<li>back end</li>
+					<li>scripting</li>
+					<li>tools</li>
+				</ul>
+				<div>
+					<a href="https://github.com/Amaranthos"><i className="fa fa-github-square"/></a>
+					<a href="https://bitbucket.org/Amaranthos"><i className="fa fa-bitbucket-square"/></a>
+				</div>
 			</div>
 		);
 	}
