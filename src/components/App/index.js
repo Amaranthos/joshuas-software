d797761ce1a72945b5b383a5446f277070146cc0
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2017-12-10 13:47:24 +1000

    Implement react-router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 import './style.css';
 
 import Home from "../Home";
+import Blog from "../Blog";
+import Portfolio from "../Portfolio";
+import Resume from "../Resume";
 
 export default class App extends Component {
 	render() {
 		return (
 			<div className="app">
 				<header>
-					<h1>joshuas.software</h1>
+					<Link to="/"><h1>joshuas.software</h1></Link>
 					<nav>
-						<a href="#">blog</a>
-						<a href="#">portfolio</a>
-						<a href="#">resume</a>
+						<Link to="/blog">blog</Link>
+						<Link to="/portfolio">portfolio</Link>
+						<Link to="/resume">resume</Link>
 					</nav>
 				</header>
 				<main>
-					<Home />
+					<Route exact path="/" component={Home} />
+					<Route path="/blog" component={Blog} />
+					<Route path="/portfolio" component={Portfolio} />
+					<Route path="/resume" component={Resume} />
 				</main>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { reducers } from "./reducers";
 
+const Root = ({ store }) => (
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
+);
+
 ReactDOM.render(
-	<Provider store={createStore(reducers)}>
-		<App />
-	</Provider>, document.getElementById('root')
+	<Root store={createStore(reducers)} />, document.getElementById('root')
 );
 registerServiceWorker();
