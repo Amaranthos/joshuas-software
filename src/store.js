072870b7525ac1ac392b1479784e3bbc37add473
Joshua Hodkinson <joshua.hodkinson@dominos.com.au>
2018-01-25 14:57:27 +1000

    Add rxjs and redux-observable

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
 
-import { reducers } from "./reducers";
+import { reducers } from './reducers';
+import { epics } from './epics';
+
+const epicMiddleware = createEpicMiddleware(epics);
 
 const store = createStore(
 	  reducers
 	, {}
-	, applyMiddleware(thunk)
+	, compose(applyMiddleware(thunk, epicMiddleware))
 );
 
 export default store;
