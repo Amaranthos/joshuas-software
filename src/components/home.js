4381069b560abfaa0d17c1f5579e7d30a83c18eb
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2018-08-25 18:18:47 +1000

    Refactor

--- src/components/home.js
+++ src/components/home.js
@@ -16,6 +16,10 @@ const Title = styled('h3')({
 	marginTop: 0
 });
 
+const LargeFaIcon = styled(FaIcon)({
+	fontSize: '2em'
+});
+
 export const Home = () => (
 	<div className='home'>
 		<Title>I write code</Title>
@@ -28,12 +32,12 @@ export const Home = () => (
 			<li>tools</li>
 		</ul>
 		<LinksDiv>
-			<IconLink href='https://github.com/amaranthos'>                           <FaIcon icon='github-square'    /></IconLink>
-			<IconLink href='https://bitbucket.org/amaranthos'>                        <FaIcon icon='bitbucket-square' /></IconLink>
-			<IconLink href='https://www.youtube.com/channel/UC4VaUP2Ob7I6avrajgBm70w'><FaIcon icon='youtube-square'   /></IconLink>
-			<IconLink href='https://www.linkedin.com/in/joshua-hodkinson'>            <FaIcon icon='linkedin-square'  /></IconLink>
-			<IconLink href='https://twitter.com/jbwhodkinson'>                        <FaIcon icon='twitter-square'   /></IconLink>
-			<IconLink href='mailto:joshua@joshuas.software'>                          <FaIcon icon='envelope-square'  /></IconLink>
+			<IconLink href='https://github.com/amaranthos'>                           <LargeFaIcon icon='github-square'    /></IconLink>
+			<IconLink href='https://bitbucket.org/amaranthos'>                        <LargeFaIcon icon='bitbucket-square' /></IconLink>
+			<IconLink href='https://www.youtube.com/channel/UC4VaUP2Ob7I6avrajgBm70w'><LargeFaIcon icon='youtube-square'   /></IconLink>
+			<IconLink href='https://www.linkedin.com/in/joshua-hodkinson'>            <LargeFaIcon icon='linkedin-square'  /></IconLink>
+			<IconLink href='https://twitter.com/jbwhodkinson'>                        <LargeFaIcon icon='twitter-square'   /></IconLink>
+			<IconLink href='mailto:joshua@joshuas.software'>                          <LargeFaIcon icon='envelope-square'  /></IconLink>
 		</LinksDiv>
 	</div>
 );
