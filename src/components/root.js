259535a77b6bedee99ebbe13f641a00d7d6791c9
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2018-08-26 16:16:35 +1000

    Add ga tracking

--- src/components/root.js
+++ src/components/root.js
@@ -3,13 +3,20 @@ import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
+import ReactGA from 'react-ga';
 
 import { App } from './app';
+
+ReactGA.initialize('UA-124565553-1');
+
+const updateGA = () => {
+	ReactGA.pageview(window.location.hash);
+}
 
 const Root = ({ store, history }) => (
 	<Provider store={store}>
 		<PersistGate loading={<div />} persistor={persistStore(store)}>
- 			<ConnectedRouter history={history}>
+ 			<ConnectedRouter onUpdate={updateGA} history={history}>
 				<App />
  			</ConnectedRouter>
  		</PersistGate>
