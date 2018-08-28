2ce351a37f9cb812bd73f1d47f0a376bc36b84cd
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2018-08-28 19:22:40 +1000

    Update GA

--- src/components/root.js
+++ src/components/root.js
@@ -3,20 +3,13 @@ import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ConnectedRouter } from 'connected-react-router';
-import ReactGA from 'react-ga';
 
 import { App } from './app';
-
-ReactGA.initialize('UA-124565553-1');
-
-const updateGA = () => {
-	ReactGA.pageview(window.location.hash);
-}
 
 const Root = ({ store, history }) => (
 	<Provider store={store}>
 		<PersistGate loading={<div />} persistor={persistStore(store)}>
- 			<ConnectedRouter onUpdate={updateGA} history={history}>
+ 			<ConnectedRouter history={history}>
 				<App />
  			</ConnectedRouter>
  		</PersistGate>
--- src/utilities/store.js
+++ src/utilities/store.js
@@ -1,3 +1,4 @@
+import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -11,6 +12,11 @@ import { epics } from '../epics';
 
 export const history = createBrowserHistory();
 const epicMiddleware = createEpicMiddleware(epics);
+
+ReactGA.initialize('UA-124565553-1', { debug: true });
+history.listen((location, action) => {
+	ReactGA.pageview(location.pathname + location.search);
+});
 
 const persistConfig = {
 	  key: 'root'
