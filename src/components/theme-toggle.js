71c2b86a7a5f651b96bba1f5fb830c25f830ecdc
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2019-02-07 22:11:12 +1000

    Clean up

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import styled, { css } from 'react-emotion';
+import styled from 'react-emotion';
 import { connect } from 'react-redux';
 
 import { Button } from './button';
 import { toggleTheme } from '../actions';
 
 const SwitchButton = styled(Button)(props => ({
-    backgroundColor: props.theme.backgroundColor,
-    borderColor: props.theme.primaryFontColor,
-    color: props.theme.primaryFontColor,
-    transition: '.4s'
+	backgroundColor: props.theme.backgroundColor,
+	borderColor: props.theme.primaryFontColor,
+	color: props.theme.primaryFontColor,
+	transition: '.4s'
 }));
 
 const ThemeToggle = ({ theme, toggleTheme }) => (
-    <SwitchButton onClick={toggleTheme}>{theme.toLowerCase()}</SwitchButton>
+	<SwitchButton onClick={toggleTheme}>{theme.toLowerCase()}</SwitchButton>
 );
 
 export const ConnectedThemeToggle = connect(
-    (state) => ({
-        theme: state.theme
-    }),
-    {
-        toggleTheme: toggleTheme
-    }
+	(state) => ({
+		theme: state.theme
+	}),
+	{
+		toggleTheme: toggleTheme
+	}
 )(ThemeToggle);
